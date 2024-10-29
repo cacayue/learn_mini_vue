28e9e787f25bcf00cb216b5255df5e28dcee7283
mafutian <a15453606@outlook.com>
2024-10-29 21:40:40 +0800

    增加emit支持传入参数方式

--- src/runtime-core/componentEmits.ts
+++ src/runtime-core/componentEmits.ts
@@ -1,12 +1,32 @@
 import { convertFirstUpperCase, hasOwn } from '../Shared/index';
 
-export function emit(instance: any, event: string) {
+export function emit(instance: any, event: string, ...args: any[]) {
   const { props } = instance;
-  const key = `on${convertFirstUpperCase(event)}`;
+  const onKey = handlerOnName(event);
+  if (hasOwn(props, onKey)) {
+    const emitFunc = props[onKey];
+    if (emitFunc) {
+      emitFunc(...args);
+    }
+  }
+  const key = handlerName(event);
   if (hasOwn(props, key)) {
     const emitFunc = props[key];
     if (emitFunc) {
-      emitFunc();
+      emitFunc(...args);
     }
   }
+}
+
+function handlerOnName(event: string) {
+  const onKey = `on${convertFirstUpperCase(event)}`;
+  return onKey;
+}
+
+function handlerName(event: string) {
+  var arr = event.split('-');
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = convertFirstUpperCase(arr[i]);
+  }
+  return `on${arr.join('')}`;
 }
