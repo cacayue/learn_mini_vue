1104715267e66c686fd04f6d8ef7dd258bac6db7
mafutian <a15453606@outlook.com>
2024-11-02 18:31:44 +0800

    Provide测试代码

---
+++ example/componentProvide/App.js
@@ -0,0 +1,35 @@
+import { h, provide, inject } from '../../lib/guide-mini-vue.esm.js';
+
+const Provide = {
+  name: 'Provide',
+  setup() {
+    provide('foo', 'fooVal');
+    provide('bar', 'barVal');
+    return {};
+  },
+  render() {
+    return h('div', {}, [h('p', {}, 'Provide'), h(Consumer)]);
+  }
+};
+
+const Consumer = {
+  name: 'Consumer',
+  setup() {
+    const foo = inject('foo');
+    const bar = inject('bar');
+    return { foo, bar };
+  },
+  render() {
+    return h('div', {}, `Consumer : -${this.foo}-${this.bar}`);
+  }
+};
+
+export default {
+  name: 'App',
+  setup() {
+    return {};
+  },
+  render() {
+    return h('div', {}, [h('p', {}, 'apiInject'), h(Provide)]);
+  }
+};
