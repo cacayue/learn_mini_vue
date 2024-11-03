f37ed7f6d49becd71b1056175e449c601d63fa42
mafutian <a15453606@outlook.com>
2024-11-03 21:36:58 +0800

    增加Props替换的测试用例

--- example/update/App.js
+++ example/update/App.js
@@ -9,18 +9,48 @@ export const App = {
       count.value++;
     };
 
+    const props = ref({
+      foo: 'foo',
+      bar: 'bar'
+    });
+
+    const onChangePropsDemo1 = () => {
+      props.value.foo = 'new-foo';
+    };
+
+    const onChangePropsDemo2 = () => {
+      props.value.foo = undefined;
+    };
+
+    const onChangePropsDemo3 = () => {
+      props.value = {
+        foo: 'foo'
+      };
+    };
+
     return {
       count,
-      onClick
+      onClick,
+      onChangePropsDemo1,
+      onChangePropsDemo2,
+      onChangePropsDemo3,
+      props
     };
   },
   render() {
     return h(
       'div',
       {
-        id: 'root'
+        id: 'root',
+        ...this.props
       },
-      [h('div', {}, `count: ${this.count}`), h('button', { onClick: this.onClick }, 'click')]
+      [
+        h('div', {}, `count: ${this.count}`),
+        h('button', { onClick: this.onClick }, 'click'),
+        h('button', { onClick: this.onChangePropsDemo1 }, '修改foo=new-foo'),
+        h('button', { onClick: this.onChangePropsDemo2 }, '修改foo=undefined'),
+        h('button', { onClick: this.onChangePropsDemo3 }, '替换整个props, 只有foo')
+      ]
     );
   }
 };
