d164799aab8fe005ad405002064b577b22f212bb
mafutian <a15453606@outlook.com>
2024-11-03 13:51:01 +0800

    完成Dom渲染的实现

---
+++ src/runtime-dom/index.ts
@@ -0,0 +1,28 @@
+import { createRender } from '../runtime-core/render';
+
+function createElement(type: string) {
+  // body
+  return document.createElement(type);
+}
+
+function patchProps(el: any, key: any, element: any) {
+  const isOn = (key: string) => /^on[A-Z]/.test(key);
+  if (isOn(key)) {
+    const event = key.slice(2).toLowerCase();
+    el.addEventListener(event, element);
+  } else {
+    el.setAttribute(key, element);
+  }
+}
+
+function insert(el: any, parent: any) {
+  parent.append(el);
+}
+
+const render: any = createRender({ createElement, patchProps, insert });
+
+export function createApp(...args: any) {
+  return render.createApp(...args);
+}
+
+export * from '../runtime-core/index';
