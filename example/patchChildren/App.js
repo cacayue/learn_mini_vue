c24c3890de4bd6b624fce5eb87e21982b2de624a
mafutian <a15453606@outlook.com>
2024-11-04 21:03:44 +0800

    Text -> Text转换用例

--- example/patchChildren/App.js
+++ example/patchChildren/App.js
@@ -1,6 +1,7 @@
 import { h } from '../../lib/guide-mini-vue.esm.js';
 import ArrayToText from './ArrayToText.js';
 import TextToArray from './TextToArray.js';
+import TextToText from './TextToText.js';
 
 export const App = {
   name: 'App',
@@ -18,7 +19,9 @@ export const App = {
         // 老的是array, 新的是text
         //h(ArrayToText),
         // 老的是Text, 新的是array
-        h(TextToArray)
+        //h(TextToArray),
+        // 老的是Text, 新的是text
+        h(TextToText)
       ]
     );
   }
