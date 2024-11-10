2401520936b99e9c2f7fe1e62afdeb890a999344
mafutian <a15453606@outlook.com>
2024-11-10 15:34:07 +0800

    基础解析的实现

---
+++ src/complier-core/src/node.ts
@@ -0,0 +1,8 @@
+export const enum NodeType {
+  INTERPOLATION,
+  SIMPLE_EXPRESSION
+}
+
+export type ParseContext = {
+  source: any;
+};
---
+++ src/complier-core/src/parse.ts
@@ -0,0 +1,66 @@
+import { NodeType, ParseContext } from './node';
+
+export function baseParse(content: string) {
+  const context = createParseContext(content);
+
+  return createRoot(parseChildren(context));
+}
+
+function parseChildren(context: ParseContext) {
+  let nodes = [];
+
+  let node = parseInterpolation(context);
+
+  nodes.push(node);
+
+  return nodes;
+}
+
+function parseInterpolation(context: ParseContext) {
+  // {{message}}
+
+  const openDelimiter = '{{';
+  const closeDelimiter = '}}';
+
+  // 9
+  const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length);
+
+  // message}}
+  advanceBy(context, openDelimiter.length);
+
+  // 7
+  const rawContentLength = closeIndex - closeDelimiter.length;
+
+  // 截取0-7：  message
+  const rawContent = context.source.slice(0, rawContentLength);
+  const content = rawContent.trim();
+
+  // 推进
+  advanceBy(context, rawContentLength + closeDelimiter.length);
+  console.log(context.source, 'close');
+
+  return {
+    type: NodeType.INTERPOLATION,
+    content: {
+      type: NodeType.SIMPLE_EXPRESSION,
+      content: content
+    }
+  };
+}
+
+// 推进： 每次处理完成对应的字符后截取掉，只保留没处理的
+function advanceBy(context: ParseContext, length: number) {
+  context.source = context.source.slice(length);
+}
+
+function createParseContext(content: string): ParseContext {
+  return {
+    source: content
+  };
+}
+
+function createRoot(children: any[]) {
+  return {
+    children: children
+  };
+}
