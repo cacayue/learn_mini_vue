e3a25e9bec6e99ca913dd882129d808d4421fc29
mafutian <a15453606@outlook.com>
2024-11-10 10:47:23 +0800

    Diff. 中间综合示例

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -155,11 +155,32 @@ import { h, ref } from '../../lib/guide-mini-vue.esm.js';
 // a,b,(c,e),f,g
 // a,b,(e,c,d),f,g
 // d 节点在老的节点中不存在，新的里面存在，所以需要创建
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+
 const prevChildren = [
   h("p", { key: "A" }, "A"),
   h("p", { key: "B" }, "B"),
   h("p", { key: "C" }, "C"),
+  h("p", { key: "D" }, "D"),
   h("p", { key: "E" }, "E"),
+  h("p", { key: "Z" }, "Z"),
   h("p", { key: "F" }, "F"),
   h("p", { key: "G" }, "G"),
 ];
@@ -167,13 +188,13 @@ const prevChildren = [
 const nextChildren = [
   h("p", { key: "A" }, "A"),
   h("p", { key: "B" }, "B"),
-  h("p", { key: "E" }, "E"),
-  h("p", { key: "C" }, "C"),
   h("p", { key: "D" }, "D"),
+  h("p", { key: "C" }, "C"),
+  h("p", { key: "Y" }, "Y"),
+  h("p", { key: "E" }, "E"),
   h("p", { key: "F" }, "F"),
   h("p", { key: "G" }, "G"),
 ];
-
 
 export default {
   name: 'ArrayToArray',
