02c94ec2916c637b6a62c026a675843d7175f33b
mafutian <a15453606@outlook.com>
2024-11-10 14:13:50 +0800

    nextTick: 测试用例更新,使用Instance示例监控el

--- example/nextTick/App.js
+++ example/nextTick/App.js
@@ -1,15 +1,23 @@
-import { h, ref } from '../../lib/guide-mini-vue.esm.js';
+import { h, ref, nextTick, getCurrentInstance } from '../../lib/guide-mini-vue.esm.js';
 
 export const App = {
   name: 'App',
   setup() {
     const count = ref(0);
-
+    const instance = getCurrentInstance();
     const onClick = () => {
       for (let i = 0; i < 100; i++) {
         count.value++;
       }
+
+      console.log(instance.vNode.el.innerText);
+
+      nextTick(() => {
+        console.log(instance.vNode.el.innerText);
+      })
+
     };
+
     return {
       count,
       onClick,
