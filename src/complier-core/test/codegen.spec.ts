2ec44cc5095100b7e11e8896743ed127c1887eb4
mafutian <a15453606@outlook.com>
2024-11-16 10:52:11 +0800

    Interpolation测试用例

--- src/complier-core/test/codegen.spec.ts
+++ src/complier-core/test/codegen.spec.ts
@@ -2,11 +2,22 @@ import { it, expect, describe } from 'vitest';
 import { baseParse } from '../src/parse';
 import { generateCode } from '../src/codegen';
 import { transform } from '../src/transform';
+import { transformExpression } from '../src/transforms/transformExpression';
 
 describe('codegen', () => {
   it('string', () => {
     const ast = baseParse('hi');
     transform(ast);
+    const { code } = generateCode(ast);
+
+    expect(code).toMatchSnapshot();
+  });
+
+  it('interpolation', () => {
+    const ast = baseParse('{{message}}');
+    transform(ast, {
+      nodeTransforms: [transformExpression]
+    });
     const { code } = generateCode(ast);
 
     expect(code).toMatchSnapshot();
