adbde1a85dcd324254d39ddef6492b1fcf692169
mafutian <a15453606@outlook.com>
2024-11-16 11:18:06 +0800

    增加element生成导入处理

--- src/complier-core/src/complierHelper.ts
+++ src/complier-core/src/complierHelper.ts
@@ -1,3 +1,7 @@
 export const TO_DISPLAY_STRING = Symbol('toDisplayString');
+export const CREATE_ELEMENT_BLOCK = Symbol('createElementBlock');
 
-export const helperNameMap = new Map([[TO_DISPLAY_STRING, 'toDisplayString']]);
+export const helperNameMap = new Map([
+  [TO_DISPLAY_STRING, 'toDisplayString'],
+  [CREATE_ELEMENT_BLOCK, 'createElementBlock']
+]);
