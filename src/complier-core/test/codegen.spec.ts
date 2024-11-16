cbc9df361c3131324db2506b4bfa900fd4fbbaad
mafutian <a15453606@outlook.com>
2024-11-16 11:17:21 +0800

    element生成测试

--- src/complier-core/test/codegen.spec.ts
+++ src/complier-core/test/codegen.spec.ts
@@ -3,6 +3,7 @@ import { baseParse } from '../src/parse';
 import { generateCode } from '../src/codegen';
 import { transform } from '../src/transform';
 import { transformExpression } from '../src/transforms/transformExpression';
+import { transformElement } from '../src/transforms/transformElement';
 
 describe('codegen', () => {
   it('string', () => {
@@ -17,6 +18,16 @@ describe('codegen', () => {
     const ast = baseParse('{{message}}');
     transform(ast, {
       nodeTransforms: [transformExpression]
+    });
+    const { code } = generateCode(ast);
+
+    expect(code).toMatchSnapshot();
+  });
+
+  it('element', () => {
+    const ast = baseParse('<div></div>');
+    transform(ast, {
+      nodeTransforms: [transformElement]
     });
     const { code } = generateCode(ast);
 
